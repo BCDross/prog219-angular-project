00ada84b8a59e94b31bbb83509aa4ef70c557dcd
Darren Ross <darren.ross@bellevuecollege.edu>
2020-08-13 21:51:18 -0700

    Final attempt to recover.

--- kanbanweb/src/app/archived-tasks/archived-tasks.component.ts
+++ kanbanweb/src/app/archived-tasks/archived-tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-archived-tasks',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archived-tasks.component.scss']
 })
 export class ArchivedTasksComponent implements OnInit {
+  tasksByStatus: Map<string, Task[]> = new Map();
 
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-  }
+    this.taskService.getTasks().subscribe(tasks => {
+      for (let task of tasks) {
+        let statusList = this.tasksByStatus.get(task.Status);
+        if (statusList === undefined) {
+          statusList = [];
+          this.tasksByStatus.set(task.Status, statusList);
+        }
+        statusList.push(task);
+      }
 
+      for (let status of this.tasksByStatus.keys()) {
+        let statusList = this.tasksByStatus.get(status);
+        statusList.sort((a, b) => a.TaskName.localeCompare(b.TaskName));
+      }
+    });
+  }
 }
